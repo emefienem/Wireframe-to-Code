2dec1d89c9a1b0d6ee49cb404a13e1254251e73b
emefienem <emefienemmichael@gmail.com>
2025-03-21 17:47:46 +0100

    Initial commit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,22 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+// import 'dotenv/config';
+// import { defineConfig } from 'drizzle-kit';
 
-export default defineConfig({
+// export default defineConfig({
 
-    schema: './configs/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.NEXT_PUBLIC_NEON_DB_CONNECTION_STRING!,
-    },
-});
+//     schema: './configs/schema.ts',
+//     dialect: 'postgresql',
+//     dbCredentials: {
+//         url: process.env.NEXT_PUBLIC_NEON_DB_CONNECTION_STRING!,
+//     },
+// });
+
+export default {
+  dialect: "postgresql",
+  schema: "./configs/schema.ts",
+  out: "./drizzle",
+
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_NEON_DB_CONNECTION_STRING,
+    connectionString: process.env.NEXT_PUBLIC_NEON_DB_CONNECTION_STRING,
+  },
+};
