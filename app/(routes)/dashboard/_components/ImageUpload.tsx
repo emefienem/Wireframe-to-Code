553fc2027b35a77e1493acb45f429dd0e1dc9f43
emefienem <emefienemmichael@gmail.com>
2025-03-23 10:45:18 +0100

    Image upload

--- app/(routes)/dashboard/_components/ImageUpload.tsx
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -1,32 +1,106 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { CloudUpload, X } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { CloudUpload, WandSparkles, X } from "lucide-react";
 import Image from "next/image";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { storage } from "@/configs/firebaseConfig";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 const ImageUpload = () => {
+  const AIModel = [
+    {
+      name: "Google Gemini",
+      icon: "/google.png",
+    },
+    {
+      name: "LLama By Meta",
+      icon: "/meta.png",
+    },
+    {
+      name: "Deepseek",
+      icon: "/deepseek.png",
+    },
+  ];
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [file, setFile] = useState<any>();
+  const [model, setModel] = useState<string>();
+  const [description, setDescription] = useState<string>();
+
   const onImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
-    if (files) {
-      console.log(files[0]);
-      const imageUrl = URL.createObjectURL(files[0]);
-      setPreviewUrl(imageUrl);
+    if (files && files[0]) {
+      // const imageUrl = URL.createObjectURL(files[0]);
+      // setFile(files[0]);
+      // setPreviewUrl(imageUrl);
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = () => {
+        if (reader.result) {
+          localStorage.setItem("previewImage", reader.result as string);
+          console.log("Image saved...");
+          setPreviewUrl(reader.result as string);
+          setFile(files[0]);
+        }
+      };
     }
   };
+
+  // const onConvertToCodeButtonClick = async () => {
+  //   if (!file || !model || !description) {
+  //     return;
+  //   }
+  //   const fileName = Date.now().toString() + ".png";
+  //   const imageRef = ref(storage, "Wireframe_To_Code/" + fileName);
+  //   await uploadBytes(imageRef, file).then((resp) => {
+  //     console.log("Image uploaded...");
+  //   });
+  //   const imageUrl = await getDownloadURL(imageRef);
+  //   console.log(imageUrl);
+  // };
+  useEffect(() => {
+    const savedImage = localStorage.getItem("previewImage");
+    if (savedImage) setPreviewUrl(savedImage);
+  }, []);
+
+  const clearPreview = () => {
+    setPreviewUrl(null);
+    localStorage.removeItem("previewImage");
+  };
+
+  const onModelChange = (value: string) => {
+    setModel(value);
+    localStorage.setItem("selectedModel", value);
+  };
+
+  const onDescriptionChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setDescription(event.target.value);
+    localStorage.setItem("description", event.target.value);
+  };
+
   return (
     <div className="mt-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         {!previewUrl ? (
           <div className="p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center">
-            <CloudUpload />
+            <CloudUpload className="h-10 w-10 text-primary" />
             <h2 className="font-bold text-lg">Upload image</h2>
             <p className="text-gray-400 mt-3">
               Click Button to Select Wireframe Image
             </p>
             <div className="p-5 border border-dashed w-full flex justify-center mt-5">
               <label htmlFor="imageSelect">
-                <h2 className="p-2 bg-primary text-white rounded-md px-5 cursor-pointer">
+                <h2 className="p-2 bg-green-100 font-medium text-primary rounded-md px-5 cursor-pointer">
                   Select Image
                 </h2>
               </label>
@@ -43,7 +117,7 @@ const ImageUpload = () => {
           <div className="p-5 border border-dashed ">
             <X
               className="flex justify-end w-full cursor-pointer"
-              onClick={() => setPreviewUrl(null)}
+              onClick={clearPreview}
             />
             <Image
               src={previewUrl}
@@ -54,7 +128,43 @@ const ImageUpload = () => {
             />
           </div>
         )}
-        <div>User Text input</div>
+        <div className="p-7 border shadow-md rounded-lg">
+          <h2 className="font-semibold">Select AI Model</h2>
+          <Select onValueChange={onModelChange}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select AI model" />
+            </SelectTrigger>
+            <SelectContent>
+              {AIModel.map((model, index) => (
+                <SelectItem value={model.name} key={index}>
+                  <div className="flex gap-2 items-center">
+                    <Image
+                      alt={model.name}
+                      width={25}
+                      height={25}
+                      src={model.icon}
+                    />
+                    <h2>{model.name}</h2>
+                  </div>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <h2 className="font-bold text-lg mt-7">
+            Enter description about your webpage
+          </h2>{" "}
+          <Textarea
+            onChange={onDescriptionChange}
+            className="mt-3 h-[200px]"
+            placeholder="Write about your webpage"
+          />
+        </div>
+      </div>
+      <div className="mt-10 flex items-center justify-center">
+        <Button>
+          {" "}
+          <WandSparkles /> Convert to Code
+        </Button>
       </div>
     </div>
   );
